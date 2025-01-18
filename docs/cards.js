31a582454ad4f0252aace0549a0b9d6874bff165
mwoo-cmu <mwoo2@alumni.cmu.edu>
2025-01-17 23:08:30 -0800

    ehtmlc

--- docs/cards.js
+++ docs/cards.js
@@ -20,25 +20,17 @@ function createCard(link, image, project, position, date) {
 }
 
 const CARDS = {
-  "connect2grow": createCard("/case-study/connect2grow.html", "{0}-connect2grow.png", "Connect2Grow", "Lead UX Designer & Tech Writer", "August 2023"),
-  "genre-analysis": createCard("/case-study/genre-analysis.html", "{0}-genre_analysis.png", "Genre Analysis", "Tech Writer", "September 2024"),
-  "dehs-project-lead": createCard("/case-study/dehs-project-lead.html", "{0}-dehs_project_lead.png", "Project Lead, DEHS", "Project Lead, BRIDGEGOOD", "June-July 2024"),
-  "sat-portal": createCard("/case-study/sat-portal.html", "{0}-sat_portal.png", "SAT Portal", "UX Designer", "April 2023-April 2024"),
-  "magazine-ad": createCard("/case-study/magazine-ad.html", "{0}-brand_ad.png", "Brand & Ad Design", "UX Designer", "January-February 2024"),
-  "fcc-microsite": createCard("/case-study/fcc-microsite.html", "{0}-fcc.png", "FCC Microsite", "UX Designer", "September 2024"),
+  "connect2grow": createCard("/case-study/connect2grow.html", "card_img-connect2grow.png", "Connect2Grow", "Lead UX Designer & Tech Writer", "August 2023"),
+  "genre-analysis": createCard("/case-study/genre-analysis.html", "card_img-genre_analysis.png", "Genre Analysis", "Tech Writer", "September 2024"),
+  "dehs-project-lead": createCard("/case-study/dehs-project-lead.html", "card_img-dehs_project_lead.png", "Project Lead, DEHS", "Project Lead, BRIDGEGOOD", "June-July 2024"),
+  "sat-portal": createCard("/case-study/sat-portal.html", "card_img-sat_portal.png", "SAT Portal", "UX Designer", "April 2023-April 2024"),
+  "magazine-ad": createCard("/case-study/magazine-ad.html", "card_img-brand_ad.png", "Brand & Ad Design", "UX Designer", "January-February 2024"),
+  "fcc-microsite": createCard("/case-study/fcc-microsite.html", "card_img-fcc.png", "FCC Microsite", "UX Designer", "September 2024"),
 };
 const HIDDEN_CARDS = {
-  "portfolio-audit": createCard("/case-study/portfolio-audit.html", "{0}-portfolio_audit.png", "Portfolio Audit", "UX Designer, Accessibility", "December 2024"),
-  "type-persona": createCard("/case-study/type-persona.html", "{0}-type_persona.png", "Type Persona", "UX Designer", "October 2024"),
+  "portfolio-audit": createCard("/case-study/portfolio-audit.html", "card_img-portfolio_audit.png", "Portfolio Audit", "UX Designer, Accessibility", "December 2024"),
+  "type-persona": createCard("/case-study/type-persona.html", "card_img-type_persona.png", "Type Persona", "UX Designer", "October 2024"),
 };
-
-function formatAndJoin(cards, prefix, join) {
-  var joined = "";
-  for (var i = 0; i < cards.length; i++) {
-    joined += sprintf(cards[i], prefix) + join;
-  }
-  return joined;
-}
 
 function createCarousel() {
   var url = window.location.href;
@@ -47,10 +39,10 @@ function createCarousel() {
   var carousel = document.getElementById("cardCarousel");
   var pageCards = { ...CARDS };
   delete pageCards[pageId];
-  carousel.innerHTML = formatAndJoin(Object.values(pageCards), "carousel_img", '\n');
+  carousel.innerHTML = Object.values(pageCards).join('\n');
 }
 
 function createGrid() {
   var grid = document.getElementById("cardGrid");
-  grid.innerHTML = formatAndJoin(Object.values(CARDS), "card_img", '\n');
+  grid.innerHTML = Object.values(CARDS).join('\n');
 }
